2d19ed6fd728c0289b2922ee00d4dc143d527561
netomachado <netomachado72@gmail.com>
2021-08-05 12:58:32 -0300

    alteraçao antes do merger

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const manimalsRouter = require('./routes/manimals');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const manimalsRouter = require("./routes/manimals");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/manimals', manimalsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/manimals", manimalsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -33,11 +33,11 @@ app.use((req, res, next) => {
 app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;

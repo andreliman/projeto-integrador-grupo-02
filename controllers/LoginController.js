fcd6fc11fe3e8e696bceba26cb6d4254490c7e29
andreliman <andredelimanunes@gmail.com>
2021-09-20 14:28:46 -0300

    Alterações do Ed inclusas

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -5,11 +5,13 @@ exports.logUser = async ({ email, password }) => {
     const userBd = await LoginModel.findUserByEmail({ email });
     const user = userBd[0];
 
-    const isValidPassword = bcrypt.compareSync(password, user.password);
+    console.log(user);
     
     if (user === undefined) {
         throw new Error ("User not found");
     };
+
+    const isValidPassword = bcrypt.compareSync(password, user.password);
 
     if (!isValidPassword) {
         throw new Error ("Incorrect password");
